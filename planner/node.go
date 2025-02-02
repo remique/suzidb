4776f1b814bd48ca7efb5aac75561b45be55eb43
remique <emj1054@gmail.com>
2025-02-02 14:18:50 +0100

    Add first NodeQuery tests

---
+++ planner/node.go
@@ -0,0 +1,88 @@
+package planner
+
+import (
+	"fmt"
+
+	"example.com/suzidb/lexer"
+	"example.com/suzidb/meta"
+	"example.com/suzidb/parser"
+	"example.com/suzidb/storage"
+)
+
+// NodeQuery is a part of a SelectPlan, which will later on be passed to
+// and executor. As can be seen below, NodeQuery may be recursive, eg.
+// NodeProjection can have a source of NodeScan. Later on it might have a
+// source of IndexLookup.
+type NodeQuery interface {
+	NodeQuery()
+}
+
+// Scan performs a basic scan of a Table. Currently, filtering out
+// specific rows is not supported (eg. 'WHERE x > 5'), since we do not have
+// proper expression parsing.
+type NodeScan struct {
+	Table meta.Table
+
+	// filter: parser.Expression
+}
+
+// Projection filters out columns that need to be queried.
+type NodeProjection struct {
+	Source  NodeQuery
+	Columns []meta.Column
+}
+
+// NodeQuery marker trait implementations
+func (ns *NodeScan) NodeQuery()       {}
+func (np *NodeProjection) NodeQuery() {}
+
+type NodeBuilder struct {
+	Catalog storage.Catalog
+}
+
+func NewNodeBuilder(c storage.Catalog) *NodeBuilder {
+	return &NodeBuilder{Catalog: c}
+}
+
+func (nb *NodeBuilder) buildNode(statement parser.Statement) (NodeQuery, error) {
+	switch statement.Kind {
+	case parser.SelectKind:
+		{
+			if isAsteriskOnly(statement.SelectStatement.SelectItems) {
+				return nb.buildNodeScan(statement)
+			}
+
+			return nil, fmt.Errorf("Unsupported query")
+		}
+	default:
+		return nil, fmt.Errorf("Expected SelectKind")
+	}
+}
+
+func (nb *NodeBuilder) buildNodeScan(statement parser.Statement) (NodeQuery, error) {
+	// Get table
+	table, err := nb.Catalog.GetTable(statement.SelectStatement.From.Literal)
+	if err != nil {
+		return nil, err
+	}
+
+	return &NodeScan{Table: *table}, nil
+}
+
+// func (nb *NodeBuilder) buildNodeProjection(statement parser.Statement) (NodeQuery, error) {
+// 	// Build source
+//      // source := buildNodeScan()
+//      // columns: Get columns from statement
+
+//      return &NodeProjection{...}
+
+// 	return &NodeScan{Table: *table}, nil
+// }
+
+func isAsteriskOnly(selectItems *[]lexer.Token) bool {
+	if len(*selectItems) == 1 && (*selectItems)[0].TokenType == lexer.STAR {
+		return true
+	}
+
+	return false
+}
---
+++ planner/node_test.go
@@ -0,0 +1,52 @@
+package planner
+
+import (
+	"testing"
+
+	"example.com/suzidb/lexer"
+	"example.com/suzidb/meta"
+	"example.com/suzidb/mocks"
+	"example.com/suzidb/parser"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBuildNodeScan(t *testing.T) {
+	mockCatalog := &mocks.MockCatalog{
+		GetTableFunc: func(name string) (*meta.Table, error) {
+			return &meta.Table{
+				Name:       "mytable",
+				PrimaryKey: "id",
+				Columns: []meta.Column{
+					{Name: "id", Type: meta.IntType, Nullable: false},
+					{Name: "name", Type: meta.StringType, Nullable: true},
+				},
+			}, nil
+		},
+	}
+
+	nb := NewNodeBuilder(mockCatalog)
+
+	statement := parser.Statement{
+		Kind: parser.SelectKind,
+		SelectStatement: &parser.SelectStatement{
+			SelectItems: &[]lexer.Token{{TokenType: lexer.STAR, Literal: "*"}},
+			From:        &lexer.Token{TokenType: lexer.IDENTIFIER, Literal: "mytable"},
+		},
+	}
+
+	expected := &NodeScan{
+		Table: meta.Table{
+			Name:       "mytable",
+			PrimaryKey: "id",
+			Columns: []meta.Column{
+				{Name: "id", Type: meta.IntType, Nullable: false},
+				{Name: "name", Type: meta.StringType, Nullable: true},
+			},
+		},
+	}
+
+	res, err := nb.buildNodeScan(statement)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, res)
+}

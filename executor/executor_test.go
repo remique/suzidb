2b50905a667301fb5f97e02d17cb35bf11fc2247
remique <emj1054@gmail.com>
2025-02-05 20:58:43 +0100

    Executor: Add Select NodeScan test

--- executor/executor_test.go
+++ executor/executor_test.go
@@ -95,3 +95,81 @@ func TestExecuteInsertPlanPKAlreadyExist(t *testing.T) {
 	assert.Error(t, err)
 	assert.Equal(t, nil, res2)
 }
+
+// This test is unecessarily long, in the future change it to plain SQL query or prepare
+// mock storage.
+func TestExecuteSelectNodeScan(t *testing.T) {
+	s := storage.NewMemStorage()
+	sm := storage.NewSchemaManager(s)
+	e := NewExecutor(s, sm)
+
+	createPlan := planner.CreateTablePlan{
+		Table: meta.Table{
+			Name:       "a",
+			PrimaryKey: "b",
+			Columns: []meta.Column{
+				{Name: "col1", Type: meta.StringType},
+				{Name: "col2", Type: meta.IntType},
+			},
+		},
+	}
+	insert1plan := planner.InsertPlan{
+		Table: meta.Table{
+			Name:       "mytable",
+			PrimaryKey: "id",
+			Columns: []meta.Column{
+				{Name: "id", Type: meta.IntType},
+				{Name: "name", Type: meta.StringType},
+			},
+		},
+		Row: meta.Row{"id": "1", "name": "john"},
+	}
+	insert2plan := planner.InsertPlan{
+		Table: meta.Table{
+			Name:       "mytable",
+			PrimaryKey: "id",
+			Columns: []meta.Column{
+				{Name: "id", Type: meta.IntType},
+				{Name: "name", Type: meta.StringType},
+			},
+		},
+		Row: meta.Row{"id": "2", "name": "emily"},
+	}
+	selectPlan := planner.SelectPlan{
+		Node: &planner.NodeScan{
+			Table: meta.Table{
+				Name:       "a",
+				PrimaryKey: "b",
+				Columns: []meta.Column{
+					{Name: "col1", Type: meta.StringType},
+					{Name: "col2", Type: meta.IntType},
+				},
+			},
+		},
+	}
+
+	_, err := e.executeCreateTable(createPlan)
+	assert.NoError(t, err)
+
+	_, err = e.executeInsert(insert1plan)
+	assert.NoError(t, err)
+
+	_, err = e.executeInsert(insert2plan)
+	assert.NoError(t, err)
+
+	expected := &SelectResult{
+		Rows: []meta.Row{
+			meta.Row{"id": "1", "name": "john"},
+			meta.Row{"id": "2", "name": "emily"},
+		},
+		Columns: []meta.Column{
+			meta.Column{Name: "col1", Type: 0, Nullable: false},
+			meta.Column{Name: "col2", Type: 1, Nullable: false},
+		},
+	}
+
+	// And saving the same plan again should result in an error
+	res, err := e.executeSelect(selectPlan)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, res)
+}

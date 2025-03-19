e873f6e73988107941bb3893dd67acdd805aa357
remique <emj1054@gmail.com>
2025-03-19 22:54:41 +0100

    Repl: Support executing queries

--- cmd/repl.go
+++ cmd/repl.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
 	"fmt"
 	"io"
 	"os"
 
+	"github.com/olekukonko/tablewriter"
+
+	"example.com/suzidb/executor"
 	"example.com/suzidb/lexer"
 	"example.com/suzidb/parser"
+	"example.com/suzidb/planner"
+	"example.com/suzidb/storage"
 )
 
 const PROMPT = "> "
@@ -16,6 +20,8 @@ const PROMPT = "> "
 func Repl(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
 
+	s := storage.NewMemStorage()
+	sm := storage.NewSchemaManager(s)
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
@@ -26,16 +32,62 @@ func Repl(in io.Reader, out io.Writer) {
 		line := scanner.Text()
 		l := lexer.NewLexer(line)
 		p := parser.NewParser(*l)
+		planner := planner.NewPlanner(sm)
 
 		parsed, err := p.ParseStatement()
 		if err != nil {
-			fmt.Println("err", err)
+			fmt.Println("parse error: ", err)
 		}
 
-		// As a quick workaround we print parsed info as JSON.
-		asJson, _ := json.Marshal(parsed)
-		fmt.Println("parsed: ", string(asJson))
+		plan, err := planner.Build(*parsed)
+		if err != nil {
+			fmt.Println("plan error: ", err)
+		}
+
+		e := executor.NewExecutor(s, sm)
+		res, err := e.ExecutePlan(plan)
+		if err != nil {
+			fmt.Println("executor error: ", err)
+		}
+
+		switch x := res.(type) {
+		case *executor.CreateTableResult:
+			fmt.Println("OK")
+		case *executor.InsertResult:
+			fmt.Println("OK")
+		case *executor.SelectResult:
+			PrettyPrintSelect(x)
+		}
 	}
+}
+
+func PrettyPrintSelect(res *executor.SelectResult) {
+	// Create table
+	table := tablewriter.NewWriter(os.Stdout)
+
+	// Extract column names for header
+	headers := []string{}
+	for _, col := range res.Columns {
+		headers = append(headers, col.Name)
+	}
+	table.SetHeader(headers)
+
+	// Populate rows
+	for _, row := range res.Rows {
+		rowData := []string{}
+		for _, col := range res.Columns {
+			val, exists := row[col.Name]
+			if !exists {
+				rowData = append(rowData, "NULL")
+			} else {
+				rowData = append(rowData, fmt.Sprintf("%v", val))
+			}
+		}
+		table.Append(rowData)
+	}
+
+	// Render table
+	table.Render()
 }
 
 func main() {
